5226788e37a4055e78853606e4feebe36194aae1
tchatua <tchatua@hotmail.com>
2025-06-05 22:05:38 -0400

    Checking skills

---
+++ Scripts/a04_Exercise.js
@@ -0,0 +1,4 @@
+document.write("Welcome")   //Message that print out in the HTML
+alert("Hello");             //Pop out saying ""Hello
+console.log("It Worked");   //Output a message into the console
+

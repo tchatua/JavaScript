0686207e76ba0d18a4d323ad85295039d1303d1a
tchatua <tchatua@hotmail.com>
2025-06-05 22:05:37 -0400

    Working with the console debug

---
+++ Scripts/a03_Console_Log.js
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+console.log(4+6);
+console.log("error");
+console.log("warn");
+
+// To attached CSS on my Java code:
+console.log("%cHello World", "color:green");
+console.log("%cHello World", "color:red; background:yellow;");
+
+
